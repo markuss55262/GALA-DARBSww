9042ee4049334061a6247ab92702b1ebff094047
markuss55262 <frokersrule@gmail.com>
2025-04-29 13:23:00 +0300

    Add files via upload

---
+++ Gala darbs 9 klase Markuss/ogijs.js/ogijs.js
@@ -0,0 +1,34 @@
+
+ let x = 4;
+let y = 6;
+let z = x+y;
+x = "niks"
+console.log(z)
+
+let person = "igo zeķe"
+carname = "opels"
+price = 18282;
+
+console.log(carname + person + price)
+
+Reiz sensenos laikos dzīvoja Artūrs, viņam sapnis bija kļūt bagātam. Viņam negāja viegli, viņam nebija nauda un katru reizi 
+kad gribējās est bija jāiet uz hesburgeru ēst pārpalikusos ēdienus. Vienu dienu viņš izdomāja vienu plānu, plāns bija sekošs, apzakt
+vietējo latiņu. Viņš abzaga veikalu, un bēga no polecijas, viens no polecistiem bija kalmačs. Bēgot no polecijas pa jaunajām mājām gadījās
+omīte uz gājējceļa. Artūs ilgi nedomajot uzbrauca virsū. Kalmačs redzot visu nopitoja artura mašīnu, izkāpa arā. Artūrs devās bēgt, 
+izkāpa arā ae vilu zeķēm un aizbēga pāri sētai.
+
+const vardsInput = document.querySelector{'#vārds'}
+const vietaInput = document.querySelector{'#vieta'}
+const laiksInput = document.querySelector{'#laiks'}
+const ediensInput = document.querySelector{'#ediens'}
+const vietatekstam = document.querySelector{'p'}
+
+vietatekstam.textContent= ookkjajs
+
+
+Pūlkstenis rādīja ${laiksInput.value}
+un viņš netika pie ${ediensInput.value}
+
+
+const vardsInput = document.querySelector('#vārds')
+const vietaInput
---
+++ Gala darbs 9 klase Markuss/pogas.js
@@ -0,0 +1,16 @@
+function SetBgGreen() {
+    document.body.style.backgroundColor="green";
+}
+
+function ARTURS() {
+ Document.Body.style.backgroundColor="red";
+}
+function Niciņš(){
+    Document.Body.style.backgroundColor="yellow"
+}
+function Feokdits(){
+    Document.Body.style.backgroundColor="red"
+}
+function Mārtiņš(){
+    Document.Body.style.backgroundColor="purple"
+}
